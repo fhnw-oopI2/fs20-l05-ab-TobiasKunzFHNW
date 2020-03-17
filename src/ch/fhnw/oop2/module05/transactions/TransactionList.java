d2fcf234a5fc3631ce63fbbc492c996e6a77e674
Kunz Tobias (s) <tobias.kunz@students.fhnw.ch>
2020-03-17 09:40:29 +0100

    Solved AB5

--- src/ch/fhnw/oop2/module05/transactions/TransactionList.java
+++ src/ch/fhnw/oop2/module05/transactions/TransactionList.java
@@ -3,15 +3,15 @@ package ch.fhnw.oop2.module05.transactions;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
  * This class implements a list of transactions performed by the traders over time.
- *
  */
 public final class TransactionList {
-	
+
 	private final List<Transaction> allTransactions = new ArrayList<>();
 
 	public void addTransaction(Transaction transaction) {
@@ -25,7 +25,7 @@ public final class TransactionList {
 	/**
 	 * Returns the transactions done in the year specified.
 	 * The transactions are sorted by value (small to high).
-	 * 
+	 *
 	 * @param year The year
 	 * @return All transactions made in this year
 	 */
@@ -34,50 +34,53 @@ public final class TransactionList {
 				.filter(t -> t.getYear() == year)
 				.sorted(Comparator.comparing(Transaction::getValue))
 				.collect(Collectors.toList());
-    }
+	}
 
 	/**
 	 * Returns all the cities in which traders work.
-	 * 
+	 *
 	 * @return The cities
 	 */
 	public List<String> cities() {
 		return allTransactions.stream()
-				.map(t->t.getTrader().getCity())
+				.map(t -> t.getTrader().getCity())
 				.distinct()
 				.collect(Collectors.toList());
-    }
+	}
 
 	/**
 	 * Returns all traders from a given city sorted by name.
-	 * 
+	 *
 	 * @param city The trader's city
 	 * @return All traders from given city sorted by name
 	 */
 	public List<Trader> traders(String city) {
-        return allTransactions.stream()
+		return allTransactions.stream()
 				.filter(t -> t.getTrader().getCity().equals(city))
 				.map(Transaction::getTrader)
 				.distinct()
 				.sorted(Comparator.comparing(Trader::getName))
 				.collect(Collectors.toList());
-    }
+	}
 
 	// TODO: AB05
+
 	/**
 	 * True if there are traders in the city, false otherwise.
-	 * 
+	 *
 	 * @param city The city
 	 * @return True if there are any trader based in given city
 	 */
 	public boolean traderInCity(String city) {
-		return false;
+		return allTransactions.stream()
+				.anyMatch(t -> t.getTrader().getCity().equals(city));
 	}
 
 	// TODO: AB06
+
 	/**
 	 * Moves all traders from their city to the city specified.
-	 * 
+	 *
 	 * @param from the trader's current city
 	 * @param to   the trader's new city
 	 */
@@ -85,12 +88,13 @@ public final class TransactionList {
 	}
 
 	// TODO: AB07
+
 	/**
 	 * Returns the highest value of all transactions.
-	 * 
+	 *
 	 * @return the highest value in all the transactions
 	 */
 	public int highestValue() {
-        return 0;
+		return 0;
 	}
 }
